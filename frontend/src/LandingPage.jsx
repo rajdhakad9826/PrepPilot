ac32fcda3e52bb0761977020576479ec8ff1dfb8
Raj Dhakad <officialrajdhakad@gmail.com>
2026-07-08 11:02:27 +0530

    fix: correct invalid react-router-dom import in LandingPage

--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -1,8 +1,7 @@
 import ProfileInfoCard from "./components/Cards/ProfileinfoCard";
 import React, { useContext, useState, useEffect } from "react";
 import { APP_FEATURES, STATS, HOW_IT_WORKS_STEPS } from "./utils/data";
-import { useNavigate } from "react-router-dom";
-import { Link, something } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import {
   LuSparkles,
   LuChevronRight,
